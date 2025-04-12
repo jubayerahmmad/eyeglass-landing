515c4826f49a0d3f5b4bae6407c5af86dceef688
jubayerahmmad <zubayersami510@gmail.com>
2025-04-12 22:04:07 +0600

    Slight changes for responsiveness

--- src/components/FlashSale/FlashSale.jsx
+++ src/components/FlashSale/FlashSale.jsx
@@ -10,29 +10,25 @@ const FlashSale = () => {
     <section className="container mx-auto px-4 py-1.5">
       <SectionHeader title={"Flash Sale"} />
       {/* static countdown */}
-      <div className="lg:flex items-center justify-between text-center bg-white rounded-full py-2 px-6 shadow-sm max-w-3xl mb-6">
-        <div className="text-xl md:text-2xl font-bold text-gray-900">
+      <div className="flex items-center justify-between bg-white rounded-full py-2 px-6 shadow-sm max-w-4xl mb-6">
+        <div className="text-base md:text-2xl font-bold text-gray-900">
           Deal of The Day
         </div>
 
         <div className="flex items-center space-x-1">
-          <span className="text-sm mr-2 text-gray-950 ">End in:</span>
-
-          <div className="bg-cyan-400 rounded-md flex items-center justify-center w-10 h-10 text-white font-bold">
+          <span className="text-sm text-gray-950 ">End in:</span>
+          <p className="bg-cyan-400 rounded-md text-white font-semibold sm:p-1">
             00D
-          </div>
-
-          <div className="bg-cyan-400 rounded-md flex items-center justify-center w-10 h-10 text-white font-bold">
+          </p>
+          <p className="bg-cyan-400 rounded-md text-white font-semibold sm:p-1">
             00H
-          </div>
-
-          <div className="bg-cyan-400 rounded-md flex items-center justify-center w-10 h-10 text-white font-bold">
+          </p>
+          <p className="bg-cyan-400 rounded-md text-white font-semibold sm:p-1">
             00M
-          </div>
-
-          <div className="bg-cyan-400 rounded-md flex items-center justify-center w-10 h-10 text-white font-bold">
+          </p>
+          <p className="bg-cyan-400 rounded-md text-white font-semibold sm:p-1">
             00S
-          </div>
+          </p>
         </div>
       </div>
       <div className="px-4">
